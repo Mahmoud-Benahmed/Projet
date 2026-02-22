12d4cfc1d0a40533ab64c5348136f04f08ed4690
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-02-23 00:55:49 +0100

    feat(user-service): add user stats DTO and update related services and gateway config

---
+++ ERPSystem/ERP.UserService/Application/DTOs/UserStatsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ERP.UserService.Application.DTOs
+{
+    public class UserStatsDto
+    {
+        public int TotalUsers { get; set; }
+        public int ActiveUsers { get; set; }
+        public int DeactivatedUsers { get; set; }
+        public int CompletedProfiles { get; set; }
+    }
+}
