c7ae09339a92dd607a21fe0dfd3dea4643c50798
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-02-26 03:35:28 +0100

    feat(auth): add centralized ApiRoutes constants

---
+++ ERPSystem/ERP.AuthService/Properties/ApiRoutes.cs
@@ -0,0 +1,27 @@
+﻿namespace ERP.AuthService.Properties
+{
+    public static class ApiRoutes
+    {
+        private const string Base = "auth";
+
+        public static class Auth
+        {
+            public const string Base = $"{ApiRoutes.Base}";
+        }
+
+        public static class Roles
+        {
+            public const string Base = $"{ApiRoutes.Base}/roles";
+        }
+
+        public static class Controles
+        {
+            public const string Base = $"{ApiRoutes.Base}/controles";
+        }
+
+        public static class Privileges
+        {
+            public const string Base = $"{ApiRoutes.Base}/privileges";
+        }
+    }
+}
