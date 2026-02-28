43e3032e7d8ceb462c05b4c94ccd1f1ad3fbb01b
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-02-28 20:58:07 +0100

    chore(db): add migration for login column on UserProfile

--- ERPSystem/ERP.UserService/Migrations/20260228193757_AddLoginToUserProfile.Designer.cs
+++ ERPSystem/ERP.UserService/Migrations/20260228193757_AddLoginToUserProfile.Designer.cs
@@ -4,6 +4,7 @@ using ERP.UserService.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ERP.UserService.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260228193757_AddLoginToUserProfile")]
+    partial class AddLoginToUserProfile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
