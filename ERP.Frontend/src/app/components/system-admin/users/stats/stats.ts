dbc47f6c7a4e64c36c57780c58ccc09b0700080b
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-03-01 07:49:46 +0100

    feat(stats): add incompleted profiles stat card with navigation and styling

--- ERP.Frontend/src/app/components/system-admin/users/stats/stats.ts
+++ ERP.Frontend/src/app/components/system-admin/users/stats/stats.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MatCardContent, MatCard } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
 import { UsersService } from '../../../../services/users.service';
 import { MatProgressSpinner } from "@angular/material/progress-spinner";
 import { UserStatsDto } from '../../../../interfaces/UserProfileDto';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stats',
@@ -18,11 +19,15 @@ export class Stats implements OnInit{
       totalUsers: 0,
       activeUsers: 0,
       deactivatedUsers: 0,
-      completedProfiles: 0
+      completedProfiles: 0,
+      incompletedProfiles: 0
   };
 
   isLoading: boolean= false;
-  constructor(private userProfileService: UsersService, private snackbar: MatSnackBar) {}
+  constructor(private userProfileService: UsersService,
+              private snackbar: MatSnackBar,
+              private router: Router,
+              private cdr: ChangeDetectorRef) {}
 
   ngOnInit(){
     this.loadStats();
@@ -34,12 +39,30 @@ export class Stats implements OnInit{
       next: (stats) => {
         this.isLoading= false;
         this.stats = stats;
+        this.cdr.markForCheck();
       },
       error: () => {
         this.isLoading= false;
         this.snackbar.open("Failed to load stats.", "Dismiss", {duration: 3000});
       }
     });
+  }
+
+
+  goToDeactivated(){
+    this.router.navigate(['/users/deactivated']);
+  }
+
+  goToUsers(){
+    this.router.navigate(['/users']);
+  }
+
+  goToCompleted(){
+      this.router.navigate(['/users'], { queryParams: { status: true } });
+  }
+
+  goToIncompleted(){
+      this.router.navigate(['/users'], { queryParams: { status: false } });
   }
 
 }
