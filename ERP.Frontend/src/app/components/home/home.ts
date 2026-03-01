35bbe8b5220716e28b3a8dfa7f1a25e8fcc0ac33
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-03-01 07:50:11 +0100

    fix(home): fix page title binding and update layout

--- ERP.Frontend/src/app/components/home/home.ts
+++ ERP.Frontend/src/app/components/home/home.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UsersService } from '../../services/users.service';
 import { forkJoin } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -21,7 +22,9 @@ export class HomeComponent implements OnInit {
   userProfile: FullProfile | null=null;
   userRole: string| null= null;
 
-  constructor(private authService: AuthService, private userProfileService: UsersService) {}
+  constructor(private authService: AuthService,
+              private snackbar: MatSnackBar,
+              private userProfileService: UsersService) {}
 
   ngOnInit(): void {
     if (!this.authService.isLoggedIn()) {
@@ -33,6 +36,24 @@ export class HomeComponent implements OnInit {
     if (this.authService.UserProfile) {
       this.userProfile = this.authService.UserProfile;
       this.userRole= this.authService.Role;
+    }else{
+      forkJoin({
+                authUser: this.authService.getMe(),
+                profile: this.userProfileService.getMe(),
+              }).subscribe({
+          next: ({ authUser, profile }) => {
+            this.userProfile = {
+              ...profile,
+              mustChangePassword: authUser.mustChangePassword,
+              lastLoginAt: authUser.lastLoginAt
+            };
+            this.authService.setUserProfile(this.userProfile);
+          },
+    error: (error) => {
+        if (error.status === 0) return;
+          this.snackbar.open('Failed to login. Unexpected error', 'Dismiss', { duration: 3000 });
+        }
+      });
     }
   }
 
