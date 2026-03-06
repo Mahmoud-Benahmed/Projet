01c0c8f9de32af21dbbff9e114a94b787a2e741a
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-03-06 21:05:22 +0100

    feat(exceptions): add UserActiveException, update UserInactiveException

---
+++ ERPSystem/ERP.AuthService/Application/Exceptions/AuthUser/UserActiveException.cs
@@ -0,0 +1,12 @@
+﻿namespace ERP.AuthService.Application.Exceptions.AuthUser
+{
+    public class UserActiveException : UnauthorizedAccessException
+    {
+        public UserActiveException() : base("User already active")
+        {
+        }
+        public UserActiveException(string message) : base(message)
+        {
+        }
+    }
+}
