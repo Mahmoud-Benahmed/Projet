5676cbe13e17e2bac862e6af497fcf27051cdb99
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-03-14 03:40:22 +0100

    feat: update gateway routing and configuration

--- ERPSystem/ERP.Gateway/Program.cs
+++ ERPSystem/ERP.Gateway/Program.cs
@@ -51,47 +51,54 @@ builder.Services.AddAuthorization(options =>
               .RequireRole("SystemAdmin"));
 
     // ── Auth
-    options.AddPolicy("ManageUsers", p => p.RequireClaim("privilege", "ManageUsers"));
-    options.AddPolicy("AssignRoles", p => p.RequireClaim("privilege", "AssignRoles"));
+    options.AddPolicy("ViewUsers",      p => p.RequireClaim("privilege",    "ViewUsers"));
+    options.AddPolicy("CreateUser",     p => p.RequireClaim("privilege",    "CreateUser"));
+    options.AddPolicy("ActivateUser",   p => p.RequireClaim("privilege",    "ActivateUser"));
+    options.AddPolicy("DeactivateUser", p => p.RequireClaim("privilege",    "DeactivateUser"));
+    options.AddPolicy("UpdateUser",     p => p.RequireClaim("privilege",    "UpdateUser"));
+    options.AddPolicy("DeleteUser",     p => p.RequireClaim("privilege",    "DeleteUser"));
+    options.AddPolicy("RestoreUser",    p => p.RequireClaim("privilege",    "RestoreUser"));
+
+    options.AddPolicy("AssignRoles",    p => p.RequireClaim("privilege",    "AssignRoles"));
 
     // Audit-Log
-    options.AddPolicy("ManageAuditLogs", p => p.RequireClaim("privilege", "ManageAuditLogs"));
+    options.AddPolicy("ManageAuditLogs",p => p.RequireClaim("privilege",    "ManageAuditLogs"));
 
     // ── Clients
-    options.AddPolicy("ViewClients", p => p.RequireClaim("privilege", "ViewClients"));
-    options.AddPolicy("CreateClient", p => p.RequireClaim("privilege", "CreateClient"));
-    options.AddPolicy("UpdateClient", p => p.RequireClaim("privilege", "UpdateClient"));
-    options.AddPolicy("DeleteClient", p => p.RequireClaim("privilege", "DeleteClient"));
-    options.AddPolicy("RestoreClient", p => p.RequireClaim("privilege", "RestoreClient"));
+    options.AddPolicy("ViewClients",    p => p.RequireClaim("privilege",    "ViewClients"));
+    options.AddPolicy("CreateClient",   p => p.RequireClaim("privilege",    "CreateClient"));
+    options.AddPolicy("UpdateClient",   p => p.RequireClaim("privilege",    "UpdateClient"));
+    options.AddPolicy("DeleteClient",   p => p.RequireClaim("privilege",    "DeleteClient"));
+    options.AddPolicy("RestoreClient",  p => p.RequireClaim("privilege",    "RestoreClient"));
 
     // ── Articles
-    options.AddPolicy("ViewArticles", p => p.RequireClaim("privilege", "ViewArticles"));
-    options.AddPolicy("CreateArticle", p => p.RequireClaim("privilege", "CreateArticle"));
-    options.AddPolicy("UpdateArticle", p => p.RequireClaim("privilege", "UpdateArticle"));
-    options.AddPolicy("DeleteArticle", p => p.RequireClaim("privilege", "DeleteArticle"));
-    options.AddPolicy("RestoreArticle", p => p.RequireClaim("privilege", "RestoreArticle"));
+    options.AddPolicy("ViewArticles",   p => p.RequireClaim("privilege",    "ViewArticles"));
+    options.AddPolicy("CreateArticle",  p => p.RequireClaim("privilege",    "CreateArticle"));
+    options.AddPolicy("UpdateArticle",  p => p.RequireClaim("privilege",    "UpdateArticle"));
+    options.AddPolicy("DeleteArticle",  p => p.RequireClaim("privilege",    "DeleteArticle"));
+    options.AddPolicy("RestoreArticle", p => p.RequireClaim("privilege",    "RestoreArticle"));
 
     // ── Facturation
-    options.AddPolicy("ViewInvoices", p => p.RequireClaim("privilege", "ViewInvoices"));
-    options.AddPolicy("CreateInvoice", p => p.RequireClaim("privilege", "CreateInvoice"));
-    options.AddPolicy("ValidateInvoice", p => p.RequireClaim("privilege", "ValidateInvoice"));
-    options.AddPolicy("DeleteInvoice", p => p.RequireClaim("privilege", "DeleteInvoice"));
-    options.AddPolicy("RestoreInvoice", p => p.RequireClaim("privilege", "RestoreInvoice"));
+    options.AddPolicy("ViewInvoices",   p => p.RequireClaim("privilege",    "ViewInvoices"));
+    options.AddPolicy("CreateInvoice",  p => p.RequireClaim("privilege",    "CreateInvoice"));
+    options.AddPolicy("ValidateInvoice",p => p.RequireClaim("privilege",    "ValidateInvoice"));
+    options.AddPolicy("DeleteInvoice",  p => p.RequireClaim("privilege",    "DeleteInvoice"));
+    options.AddPolicy("RestoreInvoice", p => p.RequireClaim("privilege",    "RestoreInvoice"));
 
-    // ── Paiements
-    options.AddPolicy("ViewPayments", p => p.RequireClaim("privilege", "ViewPayments"));
-    options.AddPolicy("RecordPayment", p => p.RequireClaim("privilege", "RecordPayment"));
-    options.AddPolicy("DeletePayment", p => p.RequireClaim("privilege", "DeletePayment"));
-    options.AddPolicy("RestorePayment", p => p.RequireClaim("privilege", "RestorePayment"));
+    // ──    Paiements
+    options.AddPolicy("ViewPayments",   p => p.RequireClaim("privilege",    "ViewPayments"));
+    options.AddPolicy("RecordPayment",  p => p.RequireClaim("privilege",    "RecordPayment"));
+    options.AddPolicy("DeletePayment",  p => p.RequireClaim("privilege",    "DeletePayment"));
+    options.AddPolicy("RestorePayment", p => p.RequireClaim("privilege",    "RestorePayment"));
 
     // ── Stocks
-    options.AddPolicy("ViewStock", p => p.RequireClaim("privilege", "ViewStock"));
-    options.AddPolicy("UpdateStock", p => p.RequireClaim("privilege", "UpdateStock"));
-    options.AddPolicy("AddEntry", p => p.RequireClaim("privilege", "AddEntry"));
+    options.AddPolicy("ViewStock",      p => p.RequireClaim("privilege",    "ViewStock"));
+    options.AddPolicy("UpdateStock",    p => p.RequireClaim("privilege",    "UpdateStock"));
+    options.AddPolicy("AddEntry",       p => p.RequireClaim("privilege",    "AddEntry"));
 
     // ── Reporting
-    options.AddPolicy("ViewReports", p => p.RequireClaim("privilege", "ViewReports"));
-    options.AddPolicy("ExportReports", p => p.RequireClaim("privilege", "ExportReports"));
+    options.AddPolicy("ViewReports",    p => p.RequireClaim("privilege",    "ViewReports"));
+    options.AddPolicy("ExportReports",  p => p.RequireClaim("privilege",    "ExportReports"));
 });
 
 //////////////////////////////////////////////////
