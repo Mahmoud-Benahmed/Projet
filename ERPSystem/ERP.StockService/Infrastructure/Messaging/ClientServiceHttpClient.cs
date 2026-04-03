99e8e67e6cfc3426ea9482cdb77914b531b0e72d
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-04-03 13:17:03 +0100

    fix(stock): place the messaging directory into infrastructure directory

---
+++ ERPSystem/ERP.StockService/Infrastructure/Messaging/ClientServiceHttpClient.cs
@@ -0,0 +1,23 @@
+﻿using ERP.StockService.Application.Exceptions;
+using System.Net;
+namespace ERP.StockService.Infrastructure.Messaging;
+
+public class ClientServiceHttpClient : IClientService
+{
+    private readonly HttpClient _httpClient;
+
+    public ClientServiceHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task ExistsByIdAsync(Guid clientId)
+    {
+        var response = await _httpClient.GetAsync($"/clients/{clientId}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+            throw new ClientNotFoundException(clientId);
+
+        response.EnsureSuccessStatusCode();
+    }
+}
