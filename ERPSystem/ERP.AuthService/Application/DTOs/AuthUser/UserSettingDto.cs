f0a81e5bea1e823c5c9fbaadc5c9c38e61a42083
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-04-04 20:23:58 +0100

    feat(AuthService): add UserSettingDto and update AuthUserGetResponseDto

---
+++ ERPSystem/ERP.AuthService/Application/DTOs/AuthUser/UserSettingDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ERP.AuthService.Application.DTOs.AuthUser;
+
+public record UserSettingsRequestDto(
+    [Required] string Theme,
+    [Required] string Language
+);
+
+public class UserSettingsResponseDto(
+    string Theme,
+    string Language
+);
