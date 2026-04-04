a01d324651c5de5332298ecf8c5dd56c30dfe531
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-04-04 20:24:37 +0100

    refactor(StockService): consolidate exceptions into single Exceptions.cs file

--- ERPSystem/ERP.StockService/Application/Exceptions/Exceptions.cs
+++ ERPSystem/ERP.StockService/Application/Exceptions/Exceptions.cs
@@ -30,3 +30,17 @@ public class RetourQuantityExceedsSourceException : Exception
     {
     }
 }
+public class ClientNotFoundException : Exception
+{
+    public ClientNotFoundException(Guid id)
+        : base($"Client with id '{id}' was not found.") { }
+}
+
+public class ArticleNotFoundException : Exception
+{
+    public ArticleNotFoundException(Guid id)
+        : base($"Article with id '{id}' was not found.") { }
+
+    public ArticleNotFoundException(string code)
+        : base($"Article with code '{code}' was not found.") { }
+}
