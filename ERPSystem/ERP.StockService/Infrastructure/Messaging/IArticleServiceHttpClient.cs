f5e4fe8a27cba64dbf0fbd42a1a6e2e117595386
Mohamed Laribi <mohamedlaribi979@gmail.com>
2026-04-13 03:00:38 +0100

    refactor(stock-service): remove HTTP clients, migrate to event-driven architecture
    - Delete ArticleServiceClient, ClientServiceHttpClient, FournisseurServiceHttpClient
    - Remove HTTP-based data fetching from external services
    - Data now synced via Kafka consumers
    - Remove old migration files (replaced by new InitialCreate)

--- ERPSystem/ERP.StockService/Infrastructure/Messaging/IArticleServiceHttpClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using ERP.StockService.Application.DTOs;
-
-namespace ERP.StockService.Infrastructure.Messaging;
-
-public interface IArticleServiceHttpClient
-{
-    Task<ArticleResponseDto> GetByIdAsync(Guid id);
-    Task<PagedResultDto<ArticleResponseDto>> GetAllPagedAsync(int pageNumber = 1, int pageSize = 10);
-}
--- ERPSystem/ERP.StockService/Infrastructure/Messaging/IFournisseurServiceHttpClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using ERP.StockService.Application.DTOs;
-
-namespace ERP.InvoiceService.Infrastructure.Messaging;
-
-public interface IFournisseurServiceHttpClient
-{
-    Task<FournisseurResponseDto> GetByIdAsync(Guid id);
-    Task<PagedResultDto<FournisseurResponseDto>> GetAllPagedAsync(int pageNumber = 1, int pageSize = 10);
-}
